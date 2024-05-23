639da34466092982fac6720a13f44cbe0f962d88
timothy ubiagba <noir210397@gmail.com>
2024-05-23 04:39:45 -0700

    home

--- src/components/home/SectionLinks.tsx
+++ src/components/home/SectionLinks.tsx
@@ -4,7 +4,7 @@ const SectionLinks = ({ link }: { link: string }) => {
   return (
     <Link
       href={"/"}
-      className="w-[108px] capitalize text-white  flex justify-center items-center aspect-square rounded-full bg-primary"
+      className="w-[108px] mt-5 capitalize text-white  flex justify-center items-center aspect-square rounded-full bg-primary"
     >
       {link}
     </Link>
